fd3795e71757466122ae3f484a3567eae2776889
Maanes <malloryrodrigues687@gmail.com>
2021-03-09 17:46:10 +0100

    Exercice N°1 / N°2 & N°6

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/default", name="default_")
+ */
+
+
+class DefaultController extends AbstractController
+{
+     /**
+     * @Route("/", name="index")
+     */
+
+    public function index() : Response
+    {
+        return $this->render('default/index.html.twig',['controller_name' => 'DefaultController',]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+
+    public function contact() : Response
+    {
+        return $this->render('default/contact.html.twig');
+    }
+}
+
+
+?>
