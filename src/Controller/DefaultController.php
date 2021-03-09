71648584dded852883e08c991bd053ba9cb57c17
Maanes <malloryrodrigues687@gmail.com>
2021-03-09 18:51:27 +0100

    Exercice N°3

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,8 +20,11 @@ class DefaultController extends AbstractController
      * @Route("/", name="index")
      */
 
-    public function index() : Response
+    public function index(ContactRepository & $contactRepository) : Response
     {
+        $contact = $contactRepository->findAll();
+        $contact = $contactRepository->findByMail('test@test.com');
+
         return $this->render('default/index.html.twig',['controller_name' => 'DefaultController',]);
     }
 
@@ -26,8 +32,17 @@ class DefaultController extends AbstractController
      * @Route("/contact", name="contact")
      */
 
-    public function contact() : Response
+    public function contact(EntityManagerInterface $em) : Response
     {
+        $contact =new Contact();
+
+        $contact->setEmail('test@test.com');
+        $contact->setSubject('Ceci est un test');
+        $contact->setMessage('Un message de test, pouvant être long, ou non. Celui-ci ne l\'est pas :) .');
+
+        $em->persist($contact);
+        $em->flush();
+
         return $this->render('default/contact.html.twig');
     }
 }
