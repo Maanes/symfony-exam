91ca63f00b309204597d91b8b63fe4d35312a97a
Maanes <malloryrodrigues687@gmail.com>
2021-03-09 19:20:46 +0100

    Exercice N°5.1 / 5.2

---
+++ src/Services/ActionResolver.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Character;
+
+class ActionResolver
+{
+    private $diceThrower;
+
+    public function __construct(DiceThrower $diceThrower)
+    {
+        $this->diceThrower = $diceThrower;
+    }
+
+    /**
+     * @param Character $character1
+     * @param Character $character2
+     */
+    public function attack(Character $character1, Character $character2)
+    {
+        $testAttack = $this->diceThrower->rollHundred(1);
+
+        if ($testAttack > $character1->getStrength()) {
+            return null;
+        }
+
+        $testDefense = $this->diceThrower->rollHundred(1);
+        if ($testDefense <= $character2->getDefense()) {
+            return null;
+        }
+
+        $damageDices = $this->diceThrower->rollTwenty(6);
+
+        return array_sum($damageDices);
+    }
+}
